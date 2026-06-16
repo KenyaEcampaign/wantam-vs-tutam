c34dfc52a862e670988821ccf2558012330f1215
KenyaEcampaign <kenya.ecampaign@gmail.com>
2026-06-16 19:28:49 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Home from "./pages/Home";
 import AdminLogin from "./pages/AdminLogin";
 import AdminDashboard from "./pages/AdminDashboard";
 
-function App() {
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-
-        <Route
-          path="/admin"
-          element={<AdminLogin />}
-        />
-
-        <Route
-          path="/admin/dashboard"
-          element={<AdminDashboard />}
-        />
+        <Route path="/admin" element={<AdminLogin />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
